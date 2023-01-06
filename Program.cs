2d45b5e8af0b4b3313416d518d34e85d5d86f640
huutinhlg2d <minkyshinyu@gmail.com>
2023-01-06 13:16:50 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace chovkbot
+{
+    public class Program
+    {
+		public static Task Main(string[] args) => new Program().MainAsync();
+
+        private DiscordSocketClient _client;
+
+        public async Task MainAsync()
+        {
+            // When working with events that have Cacheable<IMessage, ulong> parameters,
+            // you must enable the message cache in your config settings if you plan to
+            // use the cached message entity.
+            var _config = new DiscordSocketConfig { MessageCacheSize = 100 };
+            _client = new DiscordSocketClient(_config);
+
+            _client.Log += Log;
+
+            //  You can assign your bot token to a string, and pass that in to connect.
+            //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
+            var token = Environment.GetEnvironmentVariable("token");
+
+            // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
+            // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
+            // var token = File.ReadAllText("token.txt");
+            // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            _client.MessageUpdated += MessageUpdated;
+            _client.Ready += () =>
+            {
+                Console.WriteLine("Bot is connected!");
+                return Task.CompletedTask;
+            };
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        private async Task MessageUpdated(Cacheable<IMessage, ulong> before, SocketMessage after, ISocketMessageChannel channel)
+        {
+            // If the message was not in the cache, downloading it will result in getting a copy of `after`.
+            var message = await before.GetOrDownloadAsync();
+            Console.WriteLine($"'{message.Content}' -> '{after.Content}'");
+        }
+
+        private Task Log(LogMessage msg)
+		{
+			Console.WriteLine(msg.ToString());
+			return Task.CompletedTask;
+		}
+	}
+}
